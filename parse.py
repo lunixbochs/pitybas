cf29725c4ba09cc74e86ac17ea72dc01ac43b2f8
Ryan Hileman <lunixbochs@gmail.com>
2011-06-30 19:03:46 -0600

    add full token+symbol list, general improvements

--- parse.py
+++ parse.py
@@ -1,13 +1,104 @@
-SYMBOLS = '+-/*:^(){}[]<>.!'
-TOKENS = [
-	'If', 'Then', 'End', 'Goto', 'While', 'Repeat', 'Lbl',
-	'Disp', 'Output', 'ClrHome',
-	'and', 'or', 'xor', 'not'
-	'>=', '<=', '->',
+# -*- coding: utf-8 -*-
+import string
+
+SYMBOLS = [
+	'+', '-', '*', '/', u'‾', '^', u'√',
+	'=', u'≠', '>', u'≥', '<', u'≤',
+	u'→', '!', u'π', '%', 'r', u'°',
+	',', '(', ')', '[', ']', '{', '}',
 ]
+
+TOKENS = [
+	'>=', '<=', '->', u'¹', u'²', u'³',
+	'and', 'Archive', 'AxesOff', 'AxesOn', 'a+bi',
+	'Boxplot',
+	'Clear Entries', 'ClockOff', 'ClockOn',
+	'ClrAllLists', 'ClrDraw', 'ClrHome', 'ClrList', 'ClrTable',
+	'Connected', 'CoordOff', 'CoordOn', 'CubicReg',
+	u'►Dec', 'Degree', 'DelVar', 'DependAsk', 'DependAuto',
+	'DiagnosticOff', 'DiagnosticOn', 'Disp', 'DispGraph',
+	'DispTable', u'►DMS', 'Dot', 'DrawF', 'DrawInv',
+	'e', 'E', 'Else', 'End', 'Eng', 'ExecLib', 'ExpReg',
+	'ExprOff', 'ExprOn',
+	'GarbageCollect', 'getDate', 'getDtFmt', 'getKey', 'getTime',
+	'getTmFmt', 'Goto', 'GridOff', 'GridOn', 'G-T',
+	'Histogram', 'Horiz', 'Horizontal',
+	'i', 'If', 'IndpntAsk', 'IndpntAuto', 'Input', 'IsClockOn',
+	u'∟', 'LabelOff', 'LabelOn', 'Lbl', 'LinReg(a+bx)', 'LinReg(ax+b)',
+	'LinRegTInt', 'LinRegTTest', 'LnReg', 'Logistic',
+	'Manual-Fit', 'Med-Med', 'ModBoxplot',
+	'n', 'nCr', 'n/d', 'Normal', 'NormProbPlot', 'nPr',
+	'or',
+	'Param', 'Pause', 'PlotsOff', 'PlotsOn', 'Pmt_Bgn', 'Pmt_End',
+	u'►Polar', 'Polar', 'PolarGC', 'prgm', 'Prompt', 'PwrReg',
+	'QuadReg', 'QuartReg',
+	'Radian', 'rand', u're^θi', 'Real', 'RecallGDB', 'RecallPic',
+	u'►Rect', 'RectGC', 'Repeat', 'Return',
+	'Scatter', 'Sci', 'Seq', 'Sequential', 'SetUpEditor', 'Simul',
+	'SinReg', 'startTmr', 'Stop', 'StoreGDB', 'StorePic',
+	'Then', 'Time', 'TInterval', 'Trace', 'T-Test', 'tvm_FV', 'tvm_I%',
+	'tvm_N', 'tvm_Pmt', 'tvm_PV',
+	'UnArchive', 'uvAxes', 'uwAxes',
+	'Vertical', 'vwAxes',
+	'Web', 'While',
+	'xor', 'xyLine',
+	'ZBox', 'ZDecimal', 'ZInteger', 'ZInterval', 'Zoom In', 'Zoom Out',
+	'ZoomFit', 'ZoomRcl', 'ZoomStat', 'ZoomSto', 'ZPrevious', 'ZSquare',
+	'ZStandard', 'ZTrig'
+]
+
+VARIABLES = [
+	'Ans',
+	'Xmin', 'Xmax', 'Ymin', 'Ymax', u'π'
+]+list(string.uppercase)
+
+FUNCTIONS = [
+	u'√', u'³√',
+	'augment', 'angle', 'ANOVA',
+	'bal', 'binomcdf', 'binompdf',
+	'checkTmr', u'χ²cdf', u'χ²pdf', u'χ²-Test', u'χ²GOF-Test',
+	'Circle', 'conj', 'cos', u'cosֿ¹', 'cosh', u'coshֿ¹', 'cumSum',
+	'dayOfWk', 'dbd', 'det', 'dim', 'DS<',
+	'e^', u'►Eff', u'Equ►String', 'expr',
+	'Fcdf', 'Fill', 'fMax', 'fMin', 'fnInt', 'For', 'fPart', 'Fpdf',
+	'Fix', 'Float', 'FnOff', 'FnOn', u'►Frac', 'Full', 'Func',
+	'gcd', 'geometcdf', 'geometpdf', 'Get', 'GetCalc', 'getDtStr',
+	'getTmStr', 'GraphStyle',
+	'identity', 'imag', 'inString', 'int', u'ΣInt', 'invNorm',
+	'invT', 'iPart', 'irr', 'IS>',
+	'lcm', 'length', 'Line', u'ΔList', u'List►matr', 'ln','log',
+	'logBASE',
+	u'Matr►list', 'max', 'mean', 'median', 'Menu', 'min',
+	'nDeriv', u'►Nom', 'normalcdf', 'normalpdf', 'not', 'npv',
+	'OpenLib', 'Output',
+	'Plot1', 'Plot2', 'Plot3', 'poissoncdf', 'poissonpdf',
+	u'ΣPrn', 'prod', 'Pt-Change', 'Pt-Off', 'Pt-On', 'Pxl-Change',
+	'Pxl-Off', 'Pxl-On', 'pxl-Test', u'P►Rx', u'P►Ry',
+	'randBin', 'randInt', 'randIntNoRep', 'randM', 'randNorm', 'real',
+	'ref', 'remainder', 'round', '*row', 'row+', '*row+', 'rowSwap',
+	'rref', u'R►Pr', u'R►Pθ',
+	'Select', 'Send', 'seq', 'setDate', 'setDtFmt', 'setTime',
+	'setTmFmt', 'Shade', u'Shadeχ²', 'ShadeF', 'ShadeNorm',
+	'Shade_t', 'sin', u'sinֿ¹', 'sinh', u'sinhֿ¹', 'solve', 'SortA',
+	'SortD', 'stdDev', u'String►Equ', 'sub', 'sum',
+	'tan', u'tanֿ¹', 'Tangent', 'tanh', u'tanhֿ¹', 'tcdf', 'Text',
+	'timeCnv', 'tpdf',
+	'variance',
+	'Z-Test',
+
+	'Pt-On', 'For',
+	'abs', 'min', 'max', 'sqrt'
+]
+
+FUNCTIONS = [func+'(' for func in FUNCTIONS]
+TOKENS += VARIABLES + FUNCTIONS
 
 TOKENS.sort()
 TOKENS.reverse()
+
+for t in TOKENS:
+	if not t[0] in SYMBOLS and not t.isalpha():
+		SYMBOLS.append(t[0])
 
 class ParseError(Exception):
        def __init__(self, msg):
@@ -18,10 +109,15 @@ class ParseError(Exception):
 
 class Parser:
 	def __init__(self, source):
-		self.source = source.replace('\r\n', '\n').replace('\r', '\n')
+		self.source = unicode(source)
 		self.length = len(source)
 		self.pos = 0
+		self.line = 0
+		self.lines = []
 		self.ast = []
+	
+	def clean(self):
+		self.source = self.source.replace('\r\n', '\n').replace('\r', '\n')
 	
 	def error(self, msg):
 		raise ParseError(msg)
@@ -36,24 +132,33 @@ class Parser:
 		while self.more():
 			char = self.source[self.pos]
 			if char in ('\n', ':'):
-				# new line
-				# this needs to reset anything line-specific
-				# like parens and quotes
-				# maybe this will just turn into a RESET code
 				self.inc()
-				pass
-			elif char in SYMBOLS:
-				print 'parsed a symbol: %s' % self.symbol()
-			elif '0' <= char <= '9':
-				print 'parsed a number: %s' % self.number()
-			elif ('a' <= char <= 'z') or ('A' <= char <= 'Z'):
-				print 'parsed a token: %s' % self.token()
-			elif char == '"':
-				print 'parsed a string: %s' % self.string()
+				self.line += 1
+				continue
 			elif char in ' \t':
 				self.inc()
+				continue
+			elif char in SYMBOLS:
+				result = self.symbol()
+			elif '0' <= char <= '9':
+				result = self.number()
+			elif ('a' <= char <= 'z') or ('A' <= char <= 'Z'):
+				result = self.token()
+			elif char == '"':
+				result = self.string()
 			else:
-				self.error('no tokenize path for: "%s"' % char)
+				self.error('could not tokenize: %s' % repr(char))
+			
+			self.add(result)
+		
+		for line in self.lines:
+			print '"' + ('", "'.join(line)) + '"'
+	
+	def add(self, token):
+		while self.line >= len(self.lines):
+			self.lines.append([])
+		
+		self.lines[self.line].append(token)
 	
 	def symbol(self):
 		token = self.token(True)
@@ -91,7 +196,7 @@ class Parser:
 
 			num += self.number(False)
 			
-			if num[-1] == '.':
+			if num[-1] == '.' or num[0] == '.':
 				pass # TODO: verify behavior of 1. and .1 on device
 
 		return num
--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-source = '''
-If A < 3
-Then
-Disp "spam"
-End
-'''
+import sys
+if len(sys.argv) < 2:
+	print 'Usage: python test.py [file]'
+	sys.exit(1)
+
+f = open(sys.argv[1], 'r')
+source = f.read().decode('utf8')
+f.close()
 
 from parse import Parser
 parser = Parser(source)
